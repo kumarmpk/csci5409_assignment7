2a0a98c4ca2576b5025d69da4fc4695489178124
manoharan <pr971100@dal.ca>
2020-08-07 12:00:44 -0300

    serverless function

---
+++ compZ/frontend/build/precache-manifest.91e2154adc51a2bc4d6c01f61711acf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75114ce33cc779fbb9def5e3ea9d0126",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9f01d8aad67d4bfcbdd",
+    "url": "/static/js/2.29e10bbc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.29e10bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed94c1aa494eb077e68",
+    "url": "/static/js/main.a6194a7f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
